cfb3f84a00cac68ac41f255616f77265ad1bda78
Daniel Duarte <danielduartefilho.df@gmail.com>
2025-09-15 21:10:54 -0300

    adicionado 10 exemplos de vccs do systemd

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ def analyze_code_files(root_dir, model_name, output_excel_path):
         print(f"Erro ao salvar o arquivo Excel: {e}")
 
 if __name__ == "__main__":
-    repo_root_directory = os.path.join("systemd", "src", "journal")
+    repo_root_directory = os.path.join("systemd2", "src", "core")
 
     if not os.path.isdir(repo_root_directory):
         print(f"Erro: Diretório base para os códigos não encontrado: '{repo_root_directory}'")
